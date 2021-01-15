a3bbfaf0be56e775150d98ebe05c157b04e594f5
addicted-cyclist <antonchikovalex@gmail.com>
2021-01-15 18:25:01 +0300

    edit assert method in test_items.py

--- test_items.py
+++ test_items.py
@@ -4,4 +4,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_page_contains_an_add_to_cart_button(browser):
     browser.get(link)
     cart_button = browser.find_element_by_xpath("//*[@class='btn btn-lg btn-primary btn-add-to-basket']")
-    assert cart_button.text == "Añadir al carrito"
+    assert cart_button, "Невозможно добавить товар в корзину"
